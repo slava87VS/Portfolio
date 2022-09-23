c73536b52444b2afebf5c7129ea9687d20bab3e7
Vjatcheslav Sukhikh <99118184+slava87VS@users.noreply.github.com>
2022-09-23 15:01:06 +0300

    Update project_2.sql

--- Миграция данных в отдельные логические таблицы и сбор на них витрины данных/project_2.sql
+++ Миграция данных в отдельные логические таблицы и сбор на них витрины данных/project_2.sql
@@ -98,78 +98,39 @@ from shipping_transfer st) as stn
 on stn.shipping_transfer_description=s.shipping_transfer_description
 where d.shippingid=s.shippingid);
 --5 задание
-CREATE TABLE shipping_status (
-shippingid BIGINT,
-status TEXT,
-state TEXT,
-shipping_start_fact_datetime TIMESTAMP,
-shipping_end_fact_datetime TIMESTAMP,
-PRIMARY KEY (shippingid));
-with ship_max as (select shippingid,
-max(case when state = 'booked' then state_datetime else null end) as shipping_start_fact_datetime,
-max(case when state = 'recieved' then state_datetime else null end) as shipping_end_fact_datetime,
-max(state_datetime) as max_state_datetime
-from shipping
-group by shippingid)
-insert into shipping_status (shippingid, status, state, shipping_start_fact_datetime, shipping_end_fact_datetime)
-select sm.shippingid, s.status, s.state, sm.shipping_start_fact_datetime, sm.shipping_end_fact_datetime
-from ship_max as sm
-left join shipping as s on sm.shippingid = s.shippingid
-and sm.max_state_datetime = s.state_datetime
-order by shippingid;
---6 задание
-create or replace view shipping_datamart as
---временная таблица для расчета полных дней доставки
-with full_day as (select shippingid, full_day_at_shipping
-from (select shippingid, date_part('day', age (shipping_end_fact_datetime, (LAG(shipping_start_fact_datetime) OVER (PARTITION BY shippingid ORDER BY shipping_start_fact_datetime)))) as full_day_at_shipping
-from (select *
-from shipping_status
-where state = 'booked'
-or state = 'recieved'
-order by shippingid, state) as q) as w
-where full_day_at_shipping is not null),
---временная таблица для is_delay
-isdelay as (select shippingid, case when shipping_end_fact_datetime > shipping_plan_datetime then 1 else 0 end as is_delay 
-from (select distinct ss.shippingid, shipping_end_fact_datetime, shipping_plan_datetime
-from shipping_status ss
-join shipping_info si
-on ss.shippingid = si.shippingid
-order by shippingid) as e
-where shipping_end_fact_datetime is not null),
---временная таблица для is_shipping_finish
-finish as (select shippingid, case when status = 'finished' then 1 else 0 end as is_shipping_finish
-from (select shippingid, state_datetime, status, ROW_NUMBER() OVER(PARTITION BY shippingid order by state_datetime desc)
-from shipping) as q
-where row_number = 1),
---временная таблица для delay_day_at_shipping
-delay_day as (select s.shippingid,
-case when shipping_end_fact_datetime > shipping_plan_datetime then date_part ('day', age(shipping_end_fact_datetime, shipping_plan_datetime)) else 0 end as delay_day_at_shipping
-from shipping s
-left join (select shippingid, shipping_end_fact_datetime
-from shipping_status
-where state='recieved') as d
-on d.shippingid = s.shippingid),
---временная таблица для vat
-vat as (select shippingid, sa.agreement_commission, payment_amount * ( shipping_country_base_rate + agreement_rate ++ shipping_transfer_rate) as vat
-from shipping_info si
-join shipping_country_rates scr 
-on si.shipping_country_rates_id = scr.id 
-join shipping_transfer st 
-on st.id = si.shipping_transfer_id 
-join shipping_agreement sa 
-on sa.agreementid = si.agreementid)
---скрипт для заполнения view
-select s.shippingid, vendorid, transfer_type, full_day_at_shipping, is_delay, is_shipping_finish, delay_day_at_shipping, s.payment_amount, vat, v.agreement_commission * s.payment_amount as profit
-from shipping_info s
-left join shipping_transfer st 
-on st.id = s.shipping_transfer_id 
-join full_day as fd
-on fd.shippingid = s.shippingid
-left join isdelay isd
-on isd.shippingid = s.shippingid
-left join finish f
-on f.shippingid = s.shippingid
-left join delay_day dd
-on dd.shippingid = s.shippingid
-left join vat v
-on v.shippingid = s.shippingid;
+CREATE OR REPLACE VIEW shipping_datamart AS 
+SELECT 
+    si.shippingid,
+    si.vendorid,
+    st.transfer_type,
+    EXTRACT (DAY FROM (ss.shipping_end_fact_datetime - ss.shipping_start_fact_datetime)) AS full_day_at_shipping,
+    CASE
+        WHEN ss.shipping_end_fact_datetime IS NULL THEN NULL 
+        WHEN ss.shipping_end_fact_datetime < si.shipping_plan_datetime THEN 0
+        ELSE 1
+    END AS is_delay,
+    CASE
+        WHEN ss.status = 'finished' THEN 1
+        ELSE 0
+    END AS is_shipping_finish,
+    CASE
+        WHEN ss.shipping_end_fact_datetime > si.shipping_plan_datetime THEN EXTRACT (DAY FROM (ss.shipping_end_fact_datetime - si.shipping_plan_datetime))
+        ELSE 0
+    END AS delay_day_at_shipping,
+    si.payment_amount AS payment_amount,
+    si.payment_amount * (scr.shipping_country_base_rate + sa.agreement_rate + st.shipping_transfer_rate) AS vat,
+    si.payment_amount * sa.agreement_commission AS profit
+FROM 
+    public.shipping_info AS si
+JOIN
+    shipping_transfer AS st
+        USING (transfer_type_id)
+JOIN 
+    shipping_status AS ss
+        USING (shippingid)
+JOIN 
+    shipping_country_rates AS scr
+        USING (shipping_country_id)
+JOIN 
+    shipping_agreement AS sa
+        USING (agreementid);
